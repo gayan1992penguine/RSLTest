b78912622fefea53e256b8530a33bbf922002c9d
gayan <56572323+gayan1992penguine@users.noreply.github.com>
2021-05-22 03:14:50 +0530

    Add files via upload

---
+++ RSLTest/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace RSLTest
+{
+    partial class Student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Student));
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.dtStudents = new System.Windows.Forms.DataGridView();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PhoneNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnUpload.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnUpload.Location = new System.Drawing.Point(296, 407);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(148, 43);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDisplay.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDisplay.Location = new System.Drawing.Point(448, 407);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(146, 43);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // dtStudents
+            // 
+            this.dtStudents.AllowUserToAddRows = false;
+            this.dtStudents.AllowUserToDeleteRows = false;
+            this.dtStudents.AllowUserToResizeColumns = false;
+            this.dtStudents.AllowUserToResizeRows = false;
+            this.dtStudents.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dtStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FirstName,
+            this.LastName,
+            this.PhoneNo});
+            this.dtStudents.Location = new System.Drawing.Point(25, 25);
+            this.dtStudents.Name = "dtStudents";
+            this.dtStudents.ReadOnly = true;
+            this.dtStudents.RowTemplate.Height = 24;
+            this.dtStudents.Size = new System.Drawing.Size(726, 361);
+            this.dtStudents.TabIndex = 2;
+            // 
+            // FirstName
+            // 
+            this.FirstName.DataPropertyName = "First_Name";
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.DataPropertyName = "Last_Name";
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // PhoneNo
+            // 
+            this.PhoneNo.DataPropertyName = "Phone_No";
+            this.PhoneNo.HeaderText = "Phone No";
+            this.PhoneNo.Name = "PhoneNo";
+            this.PhoneNo.ReadOnly = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Location = new System.Drawing.Point(602, 407);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(146, 43);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Student
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(771, 462);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dtStudents);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnUpload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Student";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Students Data";
+            ((System.ComponentModel.ISupportInitialize)(this.dtStudents)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.DataGridView dtStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNo;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ RSLTest/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace RSLTest
+{
+    public partial class Student : Form
+    {
+        public Student()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = MessageBox.Show("Do You Want to Upload Data?");
+                if (result == DialogResult.OK)
+                {
+                    this.Close();
+                    DataTable dt = new DataTable();
+                    using (StreamReader sr = new StreamReader(@"C:\Users\pc\Documents\Visual Studio 2012\Students.csv"))
+                    {
+                        string[] headers = sr.ReadLine().Split(',');
+                        foreach (string header in headers)
+                        {
+                            dt.Columns.Add(header);
+                        }
+                        while (!sr.EndOfStream)
+                        {
+                            string[] rows = sr.ReadLine().Split(',');
+                            DataRow dr = dt.NewRow();
+                            for (int i = 0; i < headers.Length; i++)
+                            {
+                                dr[i] = rows[i];
+                            }
+                            dt.Rows.Add(dr);
+                        }
+                    }
+
+                    using (SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-GDJP6SGB\SQLEXPRESS;Initial Catalog=Students;Integrated Security=SSPI;"))
+                    {
+                        using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                        {
+                            sqlBulkCopy.DestinationTableName = "dbo.Students_Data";
+                            con.Open();
+                            sqlBulkCopy.WriteToServer(dt);
+                            con.Close();
+                        }
+                        MessageBox.Show("Save successfull");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-GDJP6SGB\SQLEXPRESS;Initial Catalog=Students;Integrated Security=SSPI;"))
+                {
+                    string sql = "SELECT * FROM Students_Data";
+                    SqlDataAdapter dataadapter = new SqlDataAdapter(sql, con);
+                    DataSet ds = new DataSet();
+                    con.Open();
+                    dataadapter.Fill(ds, "Students_Data");
+                    con.Close();
+                    dtStudents.DataSource = ds;
+                    dtStudents.DataMember = "Students_Data";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dtStudents.DataSource = null;
+                dtStudents.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
